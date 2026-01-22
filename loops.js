c59530ddf35dd3bbd8e165d9d356c3ba9ff6b93c
jikadrariddhi73593-ui <jikadrariddhi73593@gmail.com>
2026-01-22 16:34:16 +0530

    loop

---
+++ loops.js
@@ -0,0 +1,113 @@
+// loops --repeating code blocks
+// 1 2 3 4 5 6
+// for loop
+// why use for loop? when you know how many times you want to repeat a block
+ // print 1 to 10 -> console.log(1); console.log(2); console.log(10);
+// kaya thi javanu chhe --> kaya sudhi javanu chhe --> kevi rite javanu chhe
+// for (ex. 1> 50 -> increment by 1)
+//(start; end; change)
+// let i
+// for (let i = 1; i <= 10 ; i++) {
+// console.log(i);
+// }
+// for(let i= 20; i >=10; i--){
+// }
+// console.log(i)
+
+
+// while loop --do while loop
+// kaya thi javanu chhe --> Kaya rokavanu chhe --> Kevi rite javanu chhe
+// while (ex. 1> Hello world -> stop when condition false)
+// start
+// while(end) {
+// code
+// change
+// }
+
+// example1
+// let j = 10;
+// while (j <= 20) {
+// console.log("j:", j);
+// j++;
+// }
+
+//do-while loop
+
+// example2
+// let p = 1;
+
+// do {
+//   console.log(p);
+//   p++;
+// } while (p <= 5);
+
+
+// examples(sir)
+// loop
+// <---------------111--------------->
+
+// let k = 10;
+// while (k <= 20) {
+//   console.log(k);
+//   k++;
+// }
+
+// <-------------222222------------>
+
+// let k = 10;
+// while (k <= 20) {
+//   console.log(k);
+//   k++;
+// }
+// <-----------3333333333---------------->
+
+// let ao = 6;
+// while (ao > 1) {
+//   console.log(ao);
+//   ao--;
+// }
+
+// <--------------444444-------------->
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log("Hello");
+//   i++;
+// }
+
+// <--------------5555-------------->
+
+// let d = 20;
+// while (d >= 20 && d <= 25) {
+//   console.log(d);
+//   d++;
+// }
+// <-----------------6666--------------------->
+
+// let q = 50;
+// while (q <= 60) {
+//   console.log(q);
+//   q++;
+// }
+
+
+
+//do while loop
+// do {} while (end);
+// start
+// do{
+// code
+// change}
+// while(end);
+let g = 10;
+do {
+console.log("do while ", g);
+g++;
+} while (g <= 20);
+let h = 10;
+do {
+console.log("do while:", h);
+h--;
+} while (h >= 1);
+
+
