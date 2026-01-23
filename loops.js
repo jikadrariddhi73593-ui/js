aef5a8c0d7ea45b6ff3bb5a685941ac68c152cfc
jikadrariddhi73593-ui <jikadrariddhi73593@gmail.com>
2026-01-23 15:15:19 +0530

    Add files via upload
    loop

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@
 // 1 2 3 4 5 6
 // for loop
 // why use for loop? when you know how many times you want to repeat a block
- // print 1 to 10 -> console.log(1); console.log(2); console.log(10);
+// print 1 to 10 -> console.log(1); console.log(2); console.log(10);
 // kaya thi javanu chhe --> kaya sudhi javanu chhe --> kevi rite javanu chhe
 // for (ex. 1> 50 -> increment by 1)
 //(start; end; change)
@@ -99,15 +99,167 @@
 // code
 // change}
 // while(end);
-let g = 10;
-do {
-console.log("do while ", g);
-g++;
-} while (g <= 20);
-let h = 10;
-do {
-console.log("do while:", h);
-h--;
-} while (h >= 1);
+// let g = 10;
+// do {
+// console.log("do while ", g);
+// g++;
+// } while (g <= 20);
+// let h = 10;
+// do {
+// console.log("do while:", h);
+// h--;
+// } while (h >= 1);
 
 
+
+// <---------break--------->
+// example1
+// for(let k =1; k<=201; k++)
+// {
+//     console.log(k);
+//     if(k === 32)
+//     {
+//         break;
+//     }
+// }
+
+
+// example2
+// for(let k =10; k<=20; k++)
+// {
+//     console.log(k);
+//     if(k === 15)
+//     {
+//         break;
+//     }
+// }
+
+//<---------------continue---------->
+// example1
+// for(let a=1; a<=10; a++)
+// {
+//     if( a=== 5)
+//     {
+//          continue;
+//     }
+//     console.log(a);
+// }
+
+// exampl2
+// for(let a=100; a<=110; a++)
+// {
+//     if( a=== 109)
+//     {
+//          continue;
+//     }
+//     console.log(a);
+// }
+
+
+
+// Example 1:
+// for(let q=1; q<=10; q++)
+// {
+//     console.log(q)
+// }
+
+// Example 2:
+// let i = 10;  
+
+// while (i >= 1) {
+//   console.log(i);
+//   i--;         
+// }
+
+// Example 3:
+
+// for(let i=1; i<=20; i++){
+//     if(i%2 ===0){
+//     console.log(i);
+//     }
+// }
+
+// example 4:
+
+// let i = 1;
+// while (i <= 15) {
+//     if (i % 2 !== 0) 
+//     {
+//         (i % 2 === 1)
+//         console.log(i);
+//     }
+//     i++;
+
+
+// example5:
+
+// for(let i=1; i<=10; i++){
+//     console.log(`5 x ${i} = ${5*i}`);
+// }
+
+//example 6:
+// let sum = 0;
+// for(let i=1; i<=100; i++){
+//     // let sum =0;
+//     sum = sum + i;
+//     // console.log(sum);
+// }
+// console.log(sum);
+
+
+// example 7:
+// for(let i=1; i<=50; i++){
+//     if(i%3 === 0){
+//         console.log(i);
+//     }
+// }
+
+//example 8
+// let num = prompt("give a number");
+// for(let i=1; i <=num; i++){
+//     if(i%2 ===0){
+//         console.log(i, " is even");
+//     }else{
+//         console.log(i, " is odd");
+//     }
+// }
+
+//example 9 
+// for(let i=1; i<=100; i++){
+//     if(i%3 ===0 || i%5 ===0){
+//         console.log(i);
+//     }
+// }
+
+// example 10
+// for(let i=1; i<=100; i++){
+//     console.log(i);
+//     if(i%7 ===0){
+//         break;
+//     }
+// }
+
+// example 11
+// for(let i=1; i<=20; i++){
+//     if(i%3 ===0){
+//         continue;
+//     }
+//     console.log(i);
+
+// }
+
+// example 12
+// count = 0;
+// for(let i =1; i<=100; i++)
+//     {
+//         if(i%2 ===1)
+//         {
+//             count++;
+//             console.log(i)
+//         }
+//         console.log(i);
+//         if(count ===5)
+//         {
+//             break;
+//         }
+//     }
